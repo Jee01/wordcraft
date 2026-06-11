bd8712c8aa2fb88b8f47e86b4c074acdad300062
Jee01 <130762681+Jee01@users.noreply.github.com>
2026-06-11 19:51:25 +0900

    2026-06-11

--- src/main/java/com/example/wordcraft/Controller/VocaController.java
+++ src/main/java/com/example/wordcraft/Controller/VocaController.java
@@ -2,22 +2,39 @@ package com.example.wordcraft.Controller;
 
 import com.example.wordcraft.Service.VocaService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/voca")
+@RequestMapping("/api/vocab")
 public class VocaController {
     private final VocaService vocaService;
 
     public VocaController(VocaService vocaService) {this.vocaService = vocaService;}
 
-    @GetMapping("/createVoca") //반환 타입 이후 변경
+    @PostMapping //반환 타입 이후 변경
     public ResponseEntity<Map<String, String>> createVoca() {
 
+        return null;
+    }
+    @GetMapping
+    public ResponseEntity<Map<String, String>> getVocaList() {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Map<String, String>> getVocaById(@PathVariable String id) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Map<String, String>> updateVoca(@PathVariable String id, @RequestBody Map<String, String> voca) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Map<String, String>> deleteVoca(@PathVariable String id) {
         return null;
     }
 }
